166448da951a13fe940f795b7f426a6e111ade64
totoro <totoro.fu@webeye.com>
2024-08-01 11:51:50 +0800

    代码优化

--- 11_Decorators/class/02_log.ts
+++ 11_Decorators/class/02_log.ts
@@ -1,11 +1,13 @@
 // 类装饰器：日志记录，对所有类里面的方法做修改
-function logMethods(target: any) {
-  const originalMethods = Object.getOwnPropertyNames(target.prototype);
+function logMethods(constructor: any) {
+  // prototype只是构造函数的属性而已，不是其[[Prototype]]
+  const _proto_ = constructor.prototype as any;
+  const originalMethods = Object.getOwnPropertyNames(_proto_);
 
   originalMethods.forEach((methodName) => {
-    const originalMethod = target.prototype[methodName];
+    const originalMethod = _proto_[methodName];
     // 用新的方法替换原始方法
-    target.prototype[methodName] = function (...args: any[]) {
+    _proto_[methodName] = function (...args: any[]) {
       console.log(`Calling method ${methodName} with arguments ${JSON.stringify(args)}`);
       const start = Date.now();
       //  在类装饰器中，装饰器内部的 this 指向的是被装饰的类实例。
@@ -33,3 +35,14 @@ class Calculator {
 const calc = new Calculator();
 console.log(calc.add(3, 4)); // 输出: Calling method add with arguments [3,4]，Method add executed in 0 ms，7
 console.log(calc.subtract(10, 5)); // 输出: Calling method subtract with arguments [10,5]，Method subtract executed in 0 ms，5
+
+// function Person() {
+//   this.name = "fk";
+//   this.age = 25;
+// }
+
+// const person = new Person();
+
+// console.log(Reflect.getPrototypeOf(Person) === Person.prototype); //false
+// console.log(Reflect.getPrototypeOf(person) === Person.prototype); // true
+// console.log(Reflect.getPrototypeOf(Person) === Function.prototype); //true
